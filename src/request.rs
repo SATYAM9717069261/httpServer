24bb1682b5446859a41e8c4c05d8cbc86b5483f0
Satyam Mandal <satyam.mandal.77@gmail.com>
2025-09-22 01:04:01 +0530

    Header and request parsing Done

--- src/request.rs
+++ src/request.rs
@@ -7,20 +7,22 @@ pub struct RequestLine {
     pub http_version: String,
 }
 
-pub struct Request {
-    pub RequestLine: RequestLine,
-    pub Headers: HashMap<String, String>,
-    pub Body: Bytes<u8>,
+fn validatehttp(nam: &str) -> bool {
+    return nam == "HTTP/1.1";
 }
-
 impl RequestLine {
-    pub fn from_str(line: &str) -> Result<Self, &'static str> {
+    fn request_parsing(line: &str) -> Result<Self, &'static str> {
         let parts: Vec<&str> = line.trim().split_whitespace().collect();
         if parts.len() != 3 {
             return Err("invalid request line");
         }
         let method: String = parts[0].to_string();
         let request_target: String = parts[1].to_string();
+
+        if validatehttp(parts[2]) != true {
+            return Err("Validation Fail on http");
+        }
+
         let http_version: String = parts[2]
             .strip_prefix("HTTP/")
             .ok_or("missing HTTP prefix")?
@@ -34,8 +36,58 @@ impl RequestLine {
     }
 }
 
+#[derive(Debug)]
+pub struct Headers {
+    pub headers: HashMap<String, String>,
+}
+
+impl Headers {
+    pub fn header_parser(header_str: &str) -> Result<Self, &'static str> {
+        let mut headers: HashMap<String, String> = HashMap::new();
+        for line in header_str.split(SEPRATOR) {
+            if line.trim().is_empty() {
+                continue;
+            }
+            let parts: Vec<&str> = line.splitn(2, ":").map(|s| s.trim()).collect();
+            if parts.len() != 2 {
+                return Err("Something Wrong");
+            }
+            headers.insert(parts[0].to_string(), parts[1].to_string());
+        }
+        return Ok(Self { headers });
+    }
+}
+
+#[derive(Debug)]
+pub struct Request {
+    pub request_line: RequestLine,
+    pub headers: Headers,
+}
+const SEPRATOR: &str = "\r\n";
 impl Request {
-    pub fn RequestFromReader(line: &str) -> Result<Self, &'static str> {
-        return {};
+    pub fn request_from_reader(request: &str) -> Result<Self, &'static str> {
+        let parts: Vec<_> = request.match_indices(SEPRATOR).map(|(i, _)| i).collect();
+        let request_line: RequestLine;
+        match RequestLine::request_parsing(&request[..parts[0]]) {
+            Ok(data) => {
+                request_line = data;
+            }
+            Err(er) => {
+                panic!("request Parsing Error {:?}", er);
+            }
+        }
+        let headers: Headers;
+        match Headers::header_parser(&request[parts[0]..]) {
+            Ok(data) => {
+                headers = data;
+            }
+            Err(er) => {
+                panic!("header Parsing Error {:?}", er);
+            }
+        }
+        return Ok(Self {
+            request_line,
+            headers,
+        });
     }
 }
--- tests/request_test.rs
+++ tests/request_test.rs
@@ -1,4 +1,4 @@
-use httpServer::request::RequestLine;
+use httpServer::request::{Request, RequestLine};
 
 #[test]
 fn test_request_line_parse_root() {
@@ -23,5 +23,31 @@ fn test_request_line_parse_path() {
 fn test_request_line_parse_error() {
     let input = "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\nE";
     let err = RequestLine::from_str(input).unwrap_err();
+    assert_eq!(err, "invalid request line");
+}
+
+#[test]
+fn test_request_line_parse_root() {
+    let input =
+        "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+    let req = Request::request_from_reader(input).expect("should parse");
+    assert_eq!(req.request_line.method, "GET");
+    assert_eq!(req.request_line.request_target, "/");
+    assert_eq!(req.request_line.http_version, "1.1");
+}
+
+#[test]
+fn test_request_line_parse_path() {
+    let input = "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n";
+    let req = Request::request_from_reader(input).expect("should parse");
+    assert_eq!(req.request_line.method, "GET");
+    assert_eq!(req.request_line.request_target, "/coffee");
+    assert_eq!(req.request_line.http_version, "1.1");
+}
+
+#[test]
+fn test_request_line_parse_error() {
+    let input = "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\nE";
+    let err = Request::request_from_reader(input).unwrap_err();
     assert_eq!(err, "invalid request line");
 }
